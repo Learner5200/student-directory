497bac0ce11be268e04103a342dd507d79de42d6
Chris Cooksley <christopher.cooksley@gmail.com>
2018-09-12 17:00:06 +0100

    refactored

--- directory.rb
+++ directory.rb
@@ -1,47 +1,73 @@
-# students = [
-#   {name: "Dr. Hannibal Lecter", cohort: :november},
-#   {name: "Darth Vader", cohort: :november},
-#   {name: "Nurse Ratched", cohort: :november},
-#   {name: "Michael Corleone", cohort: :november},
-#   {name: "Alex DeLarge", cohort: :november},
-#   {name: "The Wicked Witch of the West", cohort: :november},
-#   {name: "Terminator", cohort: :november},
-#   {name: "Freddy Krueger", cohort: :november},
-#   {name: "The Joker", cohort: :november},
-#   {name: "Joffrey Baratheon", cohort: :november},
-#   {name: "Norman Bates", cohort: :november}
-# ]
+@students = []
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
 
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the name of a student"
   puts "To finish, just hit return twice"
-  students = []
   name = gets.chomp
-  while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    name = gets.chomp
-  end
-  students
-end
 
+  puts "And what was their cohort?"
+  puts "To finish, just hit return twice"
+  cohort = gets.chomp.to_sym
+
+  while !name.empty? do
+    @students << {name: name, cohort: cohort}
+    puts "Now we have #{@students.count} students."
+    puts "Please enter the name of a student"
+    name = gets.chomp
+    puts "And what was their cohort?"
+    cohort = gets.chomp.to_sym
+  end
+end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(80)
+  puts "-------------".center(80)
 end
 
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def print_students_list
+  @students.each_with_index do |student, index|
+    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)".center(80)
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students".center(80)
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+
+
+
+
+interactive_menu
